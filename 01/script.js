1e9a20fc610fd71467d55b4a8c85e76e6dd584a5
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-08-01 11:37:45 -0300

    iniciado o módulo
    concluídas as 3 primeiras aulas

---
+++ 01/script.js
@@ -0,0 +1,14 @@
+/* 
+  Encontre a solução ao problema:
+
+  Pergunte o nome do usuário e escreva a mensagem:
+  "Olá, [nome do usuário]"
+
+*/
+
+let name = prompt("Qual o seu nome?")
+
+alert(`Olá, ${name}!`)
+  
+
+
